1c2c5313add5ebd0fcb42fdf26100774a3fb12cc
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-15 13:50:49 -0300

    login, logged page(1/2)

---
+++ src/pages/logged/index.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/Header";
+
+export default function Logged() {
+  return (
+    <main>
+      <Header />
+      <section>
+        <div>
+          <h1>Carrossel</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
